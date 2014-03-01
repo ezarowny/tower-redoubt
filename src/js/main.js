f2f4736349e8ec5919fd25e35feff9c8ca095823
Riley Watkins <riley@rileywatkins.com>
2014-02-28 18:14:37 -0800

    Cleanup

--- src/js/main.js
+++ src/js/main.js
@@ -1,43 +1,39 @@
-var main = document.getElementById("main");
-
-function getCursorPosition(e) {
-    var x;
-    var y;
-        if (e.pageX != undefined && e.pageY != undefined) {
+var getCursorPosition = function(e) {
+    var x, y;
+    if (e.pageX != undefined && e.pageY != undefined) {
         x = e.pageX;
         y = e.pageY;
-    }
-    else {
+    } else {
         x = e.clientX + document.body.scrollLeft +
                 document.documentElement.scrollLeft;
         y = e.clientY + document.body.scrollTop +
                 document.documentElement.scrollTop;
     }
-
     x -= main.offsetLeft;
     y -= main.offsetTop;
-
     return [x, y];
-}
+};
 
-function draw_boundary() {
-    var main = document.getElementById("main");
-    var context = main.getContext("2d");
+var drawBoundary = function(canvas) {
+    var context = canvas.getContext('2d');
     context.moveTo(80, 80);
     context.lineTo(560, 80);
     context.lineTo(560, 400);
     context.lineTo(80, 400);
     context.lineTo(80, 80);
     context.stroke();
-}
+};
 
 var onClick = function(event) {
     var position = getCursorPosition(event);
-    var main = document.getElementById("main");
-    var context = main.getContext("2d");
-    context.fillRect(position[0], position[1], 20, 20);
+    var canvas = event.target;
+    var context = canvas.getContext('2d');
+    context.fillRect(position[0], position[1], 30, 30);
     console.log(position);
-}
+};
 
-main.addEventListener("click", onClick, false);
-draw_boundary();
+(function(document) {
+    var canvas = document.getElementById('main');
+    canvas.addEventListener('click', onClick, false);
+    drawBoundary(canvas);
+})(document);
